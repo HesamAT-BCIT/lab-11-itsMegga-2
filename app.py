6a50f4aa9cf60813140f934ed509469753dcce18
itsMegga <ronald10ho@gmail.com>
2026-04-04 17:13:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ app.register_blueprint(profile_bp)
 app.register_blueprint(api_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=5000)
